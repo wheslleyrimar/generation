9715f96e58a3cde0cb72941ca5bcb32c2b17ac73
Wheslley <wheslley_rimar@hotmail.com>
2020-03-19 15:02:58 -0300

    API Rest

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'tuffi.db.elephantsql.com',
+    port: 5432,
+    username: 'ppzozlwb',
+    password: 'MoEzF8CGmQIqpoTPbiLz4k4fC4phHTDh',
+    database: 'ppzozlwb',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+}
+
+//postgres://rixhxplf:xxjhCYVIs-IckW6hOL45BELceewL2E8J@drona.db.elephantsql.com:5432/rixhxplf'
